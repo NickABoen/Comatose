8eff5d77019487137b9c096a9542fdf0a5e1d63f
NickABoen <NickABoen@gmail.com>
2016-02-06 13:07:11 -0600

    Tuned up the input system, decoupled the position from keyboard inputs, keyboard movement now controls velocity which gets applied to everything with a position and velocity component in the new movement system. Added the HUMP plugin for Love that adds a lot of nice game state and vector functions as well as Trace.lua which adds a scrolling text output. Added two functions printNotice and printDebug that print text (as a parameter) in green and white respectively. Currently working on a simple keyboard state machine for detecting differences between presses, holds, and releases.

--- Comatose/main.lua
+++ Comatose/main.lua
@@ -1,45 +1,100 @@
---hi nick I wrote this in vim ;)
-function spellingCorrection(str)
-    return string.gsub(str, "emacs", "vim")
-end
+--------------------------------------------------
+--                    Comatose                  --
+--------------------------------------------------
+local debug = true
 
+vector = require('hump/vector')
 local Secs = require('secs')
 local world = Secs.new()
+require 'trace'
+
+local fps
+
+-- Useful trace functions that manage some writing
+function printDebug(text)
+    if debug then
+        trace.print(text, trace.styles.white)
+    end
+end
+function printNotice(text)
+    if debug then
+        trace.print(text, trace.styles.green)
+    end
+end
+function printFPS()
+    if debug then
+        love.graphics.printf("Current FPS: " .. tostring(love.timer.getFPS()), 0,0, love.graphics.getWidth(), 'right')
+    end
+end
+
+-- useful "enums"
+local player_states = {neutral = "neutral", rolling = "rolling"}
 
 -- create the components
-world:addComponent("position", {x = 0, y = 0})
+world:addComponent("position", { pos = vector(0,0)})
+world:addComponent("velocity", { maxSpeed = 100, currentSpeed = 100, vec = vector(0,0)})
 world:addComponent("boundingBox", {width = 10, height = 10})
 world:addComponent("hasInput", {})
+world:addComponent("player", {state = player_states.neutral})
 world:addComponent("renderable",{})
+world:addComponent("debug",{ name = ''})
 
 -- adding an input system
 -- this system will handle processing user input
 world:addSystem("input",{
-    speed = 100,
     update = function(self, dt)
-        for entity in pairs(world:query("hasInput position")) do
-            local pos = entity.position
-
-            if love.keyboard.isDown("up") then
-                pos.y = pos.y - self.speed * dt
-            end
-
-            if love.keyboard.isDown("down") then
-                pos.y = pos.y + self.speed * dt
-            end
-
-            if love.keyboard.isDown("left") then
-                pos.x = pos.x - self.speed * dt
-            end
-
-            if love.keyboard.isDown("right") then
-                pos.x = pos.x + self.speed * dt
-            end
+        for entity in pairs(world:query("hasInput velocity")) do
+            local velocity = entity.velocity
+            local currentSpeed = entity.velocity.currentSpeed
+            local player = entity.player
 
             if love.keyboard.isDown("escape") then
                 love.event.quit()
             end
 
+            if player.state == player_states.neutral then
+            
+                if love.keyboard.isDown("up") then
+                    velocity.vec.y = -1
+                elseif love.keyboard.isDown("down") then
+                    velocity.vec.y = 1
+                else
+                    velocity.vec.y = 0
+                end
+
+                if love.keyboard.isDown("left") then
+                    velocity.vec.x = -1
+                elseif love.keyboard.isDown("right") then
+                    velocity.vec.x = 1
+                else
+                    velocity.vec.x = 0
+                end
+
+                velocity.vec = velocity.vec:normalized()
+
+                if love.keyboard.isDown("space") then
+                    player.state = player_states.rolling
+                end
+
+            elseif player.state == player_states.rolling then
+                -- the player should have no or limited actions in this state
+            elseif player.state == player_states.jumping then
+                -- the player should have no or limited actions in this state
+            end
+        end
+    end
+})
+
+-- add a "movement" system with a update callback
+-- this system updates the position components of all entities with a velocity component
+world:addSystem("movement", {
+    update = function(self, dt)
+        for entity in pairs(world:query("position velocity")) do
+           local position = entity.position
+           local vec = entity.velocity.vec
+           local speed = entity.velocity.currentSpeed
+
+           position.pos = position.pos + (vec * speed * dt)
         end
     end
 })
@@ -48,11 +103,11 @@ world:addSystem("input",{
 -- this system will handle rendering rectangles
 world:addSystem("render", {
     draw = function(self)
-        for entity in pairs(world:query("renderable position boundingBox")) do
+        for entity in pairs(world:query("renderable position boundingBox debug")) do
             love.graphics.rectangle(
                 "fill",
-                entity.position.x,
-                entity.position.y,
+                entity.position.pos.x,
+                entity.position.pos.y,
                 entity.boundingBox.width,
                 entity.boundingBox.height
             )
@@ -62,19 +117,17 @@ world:addSystem("render", {
 
 -- create a player entity at position (100, 100)
 local player = world:addEntity({
-    position = {x = 100, y = 100},
+    position = {pos = vector(100,100)},
     boundingBox = {},
     hasInput = {},
-    renderable = {}
-})
-
--- create a large, generic rectangle entity at position (200, 0)
-world:addEntity({
-    position = { x = 200},
-    boundingBox = { width = 20, height = 30}
+    renderable = {},
+    debug = {name = 'player'},
+    player = {},
+    velocity = {maxSpeed = 100, currentSpeed = 100}
 })
 
 function love.load()
+    printNotice('Trace system online.', trace.styles.green)
 end
 
 function love.update(dt)
@@ -86,13 +139,7 @@ function love.update(dt)
 end
 
 function love.draw(dt)
-    local str1 = "emacs"
-    local str2 = "emacsemacs emacs"
-    local str3 = "themacs and cheese"
-
     world:draw()
-
-    love.graphics.print(tostring(str1) .. "=" .. tostring(spellingCorrection(str1)), 0,0)
-    love.graphics.print(tostring(str2) .. "=" .. tostring(spellingCorrection(str2)), 0,10)
-    love.graphics.print(tostring(str3) .. "=" .. tostring(spellingCorrection(str3)), 0,20)
+    trace.draw()
+    printFPS()
 end
