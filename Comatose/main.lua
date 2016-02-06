37593d1fe3023db37432f7e001d3a6faa494d9bf
JakeEhrlich <jake.h.ehrlich@gmail.com>
2016-02-06 14:46:53 -0600

    we are saved
    holy fuck

--- Comatose/main.lua
+++ Comatose/main.lua
@@ -107,14 +107,14 @@ world:addSystem("render", {
         --get the renderables
         local rens = {}
         for k in pairs(world:query("renderable")) do
-          print(k)
+          --print(k)
           table.insert(rens, k)
         end
         --sort them
         table.sort(rens, function(r1, r2)
           return r1.renderable.z < r2.renderable.z
         end)
-        print(#rens)
+        --print(#rens)
         --now draw all of them
         for i, entity in ipairs(rens) do
           entity.renderable.draw(entity)
@@ -132,8 +132,8 @@ local player = world:addEntity({
       draw = function(player)
         love.graphics.rectangle(
             "fill",
-            player.position.x,
-            player.position.y,
+            player.position.pos.x,
+            player.position.pos.y,
             player.boundingBox.width,
             player.boundingBox.height
         )
