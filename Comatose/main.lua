40909bc0c87d0683fe4b52278420baed9588254d
JakeEhrlich <jake.h.ehrlich@gmail.com>
2016-02-07 00:16:14 -0600

    did the candyland

--- Comatose/main.lua
+++ Comatose/main.lua
@@ -1,68 +1,52 @@
 --------------------------------------------------
 --                    Comatose                  --
 --------------------------------------------------
-local debug = false
+debug = true
 
+Gamestate = require('hump/gamestate')
 Camera = require('hump/camera')
 vector = require('hump/vector')
 require('TiledLoader')
 Secs = require('secs')
-world = Secs.new()
 HC = require 'HC'
 require 'trace'
-
-local fps
-
--- Useful trace functions that manage some writing
-function printDebug(text)
-    if debug then
-        trace.print(text, trace.styles.white)
-    end
-end
-function printNotice(text)
-    if debug then
-        trace.print(text, trace.styles.green)
-    end
-end
-function printFPS()
-    if debug then
-        love.graphics.printf("Current FPS: " .. tostring(love.timer.getFPS()), 0,0, love.graphics.getWidth(), 'right')
-    end
-end
-
--- useful "enums"
-player_states = {neutral = "neutral", rolling = "rolling"}
-key_states = {up = 'up', down = 'down', pressed = 'pressed', released = 'released'}
-
-require 'systems'
+require 'util'
 require "AnimatedSprite"
-require 'components'
+
+function getPlayer(world)
+  for player in pairs(world:query("player")) do
+    return player
+  end
+end
+
+--read in the worlds
+local leadinWorld = require('leadin/leadin')
+
+--make the game states (ensure these are global)
+--here is the code for the leadin
+leadin = {}
+function leadin:update(dt)
+  leadinWorld:update(dt)
+end
+function leadin:draw()
+  cam:attach()
+  leadinWorld:draw()
+  cam:detach()
+  trace.draw()
+  printFPS()
+  local player = getPlayer(leadinWorld)
+  love.graphics.printf("health: " .. player.health.value, 0, 20, love.graphics.getWidth(), 'right')
+  love.graphics.printf("hunger: " .. player.hunger.value, 0, 40, love.graphics.getWidth(), 'right')
+  love.graphics.printf("glucose: " .. player.glucose.value, 0, 60, love.graphics.getWidth(), 'right')
+  love.graphics.printf("insulin: " .. player.insulin.value, 0, 80, love.graphics.getWidth(), 'right')
+end
+--next other things could come
 
 function love.load()
-  Witch = GetInstance ("WitchSprite.lua")
-  Player = GetInstance ("PlayerSprite.lua")
-
-  print(love.graphics.getBlendMode())
   printNotice('Trace system online.', trace.styles.green)
-  --love.graphics.setBackgroundColor(0, 255, 0)
-  --love.graphics.setBlendMode("alpha")
   cam = Camera(100, 100)
   cam:zoom(2.5)
-end
-
-function love.update(dt)
-    world:update(dt)
-end
-
-function love.draw(dt)
-    cam:attach()
-    world:draw()
-    cam:detach()
-    trace.draw()
-    printFPS()
-    local player = getPlayer()
-    love.graphics.printf("health: " .. player.health.value, 0, 20, love.graphics.getWidth(), 'right')
-    love.graphics.printf("hunger: " .. player.hunger.value, 0, 40, love.graphics.getWidth(), 'right')
-    love.graphics.printf("glucose: " .. player.glucose.value, 0, 60, love.graphics.getWidth(), 'right')
-    love.graphics.printf("insulin: " .. player.insulin.value, 0, 80, love.graphics.getWidth(), 'right')
+  --init the game states
+  Gamestate.registerEvents()
+  Gamestate.switch(leadin)
 end
---
+++ Comatose/util.lua
@@ -0,0 +1,18 @@
+local fps
+
+-- Useful trace functions that manage some writing
+function printDebug(text)
+    if debug then
+        trace.print(text, trace.styles.white)
+    end
+end
+function printNotice(text)
+    if debug then
+        trace.print(text, trace.styles.green)
+    end
+end
+function printFPS()
+    if debug then
+        love.graphics.printf("Current FPS: " .. tostring(love.timer.getFPS()), 0,0, love.graphics.getWidth(), 'right')
+    end
+end
